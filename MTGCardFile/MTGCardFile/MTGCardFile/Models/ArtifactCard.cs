92bf4388c326ef74db9ce00f6a35d783fcb65925
Amanda Cordray <acordray79@gmail.com>
2023-12-23 19:14:26 -0500

    one more time...

---
+++ MTGCardFile/MTGCardFile/MTGCardFile/Models/ArtifactCard.cs
@@ -0,0 +1,15 @@
+﻿namespace MTGCardFile.Models
+{
+    public class ArtifactCard : CardObject
+    {
+        public string ArtifactType { get; set; }
+        public string ArtifactEffect { get; set; }
+
+        public ArtifactCard() { }
+        public ArtifactCard(string artifactType, string artifactEffect) : base()
+        {
+            ArtifactType = artifactType;
+            ArtifactEffect = artifactEffect;
+        }
+    }
+}
---
+++ MTGCardFile/MTGCardFile/MTGCardFile/Models/CardObject.cs
@@ -0,0 +1,22 @@
+﻿namespace MTGCardFile.Models
+{
+    public class CardObject
+    {
+        public string CardName { get; set; }
+        public string SetName { get; set; }
+        public List<string> ManaType { get; set; }
+        public int CastingCost { get; set; }
+        public string CardBio { get; set; }
+
+        public CardObject() { }
+
+        public CardObject(string cardName, string setName, List<string> manaType, int castingCost, string cardBio)
+        {
+            CardName = cardName;
+            SetName = setName;
+            ManaType = manaType;
+            CastingCost = castingCost;
+            CardBio = cardBio;
+        }
+    }
+}
