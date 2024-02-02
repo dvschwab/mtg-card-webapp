85218217ec2c8717c5794d45d8ff351a562578b7
David Schwab <davidschwab784@aol.com>
2024-02-02 18:57:40 -0500

    Added an enum for mana types and a toJson() method to the Card class. Added a CardBox class to store the cards.

---
+++ MTGCardFile/MTGCardFile/MTGCardFile/Data/CardBox.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32.SafeHandles;
+using MTGCardFile.Models;
+
+namespace MTGCardFile.Data
+{
+    public class CardBox : FileStream
+    {
+        public string Filename { get; set; }
+        public string? ImageFile { get; set; }
+        public CardBox(string filename) : base(filename, FileMode.CreateNew) { }
+
+        public CardBox(string filename, string imageFile) : base(filename, FileMode.CreateNew)
+        {
+            ImageFile = imageFile;
+        }
+
+        public void AddCard (CardObject Card)
+        {
+            using(FileStream fs = new FileStream(Filename, FileMode.Open, FileAccess.Write))
+            {
+                foreach(byte ch in Card.CardName)
+                {
+                    fs.WriteByte(ch);
+                }
+            }
+        }
+
+        public void Package(string filename)
+        {
+            // Packages the CardBox files together as a compressed file
+        }
+    }
+}
--- MTGCardFile/MTGCardFile/MTGCardFile/Models/CardObject.cs
+++ MTGCardFile/MTGCardFile/MTGCardFile/Models/CardObject.cs
@@ -1,20 +1,36 @@
-﻿namespace MTGCardFile.Models
+﻿using System.Text.Json;
+
+namespace MTGCardFile.Models
 {
     public class CardObject
     {
         public string CardName { get; set; }
         public string SetName { get; set; }
-        public List<string> ManaType { get; set; }
+        public List<ManaTypes> ManaType { get; set; }
         public int CastingCost { get; set; }
         public string CardBio { get; set; }
 
-        public CardObject(string cardName, string setName, List<string> manaType, int castingCost, string cardBio)
+        public CardObject(string cardName, string setName, List<ManaTypes> manaType, int castingCost, string cardBio)
         {
             CardName = cardName;
             SetName = setName;
             ManaType = manaType;
             CastingCost = castingCost;
             CardBio = cardBio;
+        }
+
+        public enum ManaTypes
+        {
+            FOREST,
+            MOUNTAIN,
+            ISLAND,
+            PLAIN,
+            SWAMP
+        }
+
+        public string toJson() 
+        {
+            return JsonSerializer.Serialize(this);
         }
     }
 }
